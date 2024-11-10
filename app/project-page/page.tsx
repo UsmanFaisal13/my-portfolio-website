9271d4823f6e83a96243860786bfec2b2e5a33fd
UsmanFaisal13 <130107471+UsmanFaisal13@users.noreply.github.com>
2024-11-10 23:04:00 +0500

    Basic portfolio website complete

--- app/project-page/page.tsx
+++ app/project-page/page.tsx
@@ -8,19 +8,21 @@ const page = () => {
             <h1 className='text-6xl font-Darker_Grotesque mb-8'>Projects</h1>
             <div className='flex flex-row gap-4 justify-center flex-wrap w-full'>
 
-                <ProjectCard projectName="My Cool Project"
-                    projectDescription="This is an awesome project description"
-                    projectLink="https://github.com/yourproject" languages={["Nextjs", "TypeScript"]} />
-                <ProjectCard projectName="My Cool Project"
-                    projectDescription="This is an awesome project description"
-                    projectLink="https://github.com/yourproject" languages={["Nextjs", "TypeScript"]} />
-                <ProjectCard projectName="My Cool Project"
-                    projectDescription="This is an awesome project description"
-                    projectLink="https://github.com/yourproject" languages={["Nextjs", "TypeScript"]} />
-                <ProjectCard projectName="My Cool Project"
-                    projectDescription="This is an awesome project description"
-                    projectLink="https://github.com/yourproject" languages={["Nextjs", "TypeScript"]} />
-
+                <ProjectCard projectName="Figma design to Next JS Website"
+                    projectDescription="This project was given by my teachers in GIAIC
+                    to test our skills in Figma design to Next JS Website"
+                    projectLink="https://figma-to-nextjs-six.vercel.app/" languages={["Nextjs", "TypeScript", "Tailwind"]} />
+                <ProjectCard projectName="Country List"
+                    projectDescription="This project was given by my teachers in GIAIC
+                    to test our skills in Dynamic routing feature in next JS design to Next JS Website"
+                    projectLink="https://country-list-three.vercel.app/" languages={["Nextjs", "TypeScript"]} />
+                <ProjectCard projectName="Todo App"
+                    projectDescription="This is an awesome personal project description"
+                    projectLink="https://react-todo-list-flame-ten.vercel.app/" languages={["CSS", "Javascript"]} />
+                <ProjectCard projectName="My Portfolio Website"
+                    projectDescription="This project was given by my teachers in GIAIC
+                    to test our skills in Next js and design"
+                    projectLink="https://usman-faisal.vercel.app/" languages={["Nextjs", "TypeScript", "Tailwind"]} />
 
 
             </div>
